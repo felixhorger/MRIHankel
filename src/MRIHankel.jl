5c59d70b29bd7c0261d1d15ca29e7ac4f274e177
Felix Horger <felix.horger@kcl.ac.uk>
2022-10-16 16:20:23 +0100

    Improved performance by restructuring for-loops. Better modularisation.

--- src/MRIHankel.jl
+++ src/MRIHankel.jl
@@ -4,9 +4,46 @@ module MRIHankel
 	export hankel_matrix
 
 	using Base.Cartesian
-	
+
+	function hankel_matrix(data::AbstractArray{T, N}, kernelsize::NTuple{D, Integer}, neighbours::NTuple{M, CartesianIndex{D}}) where {T<:Number, N, D, M}
+		@assert N > 1
+		@assert N == D + 1
+
+		# Get dimensions
+		data_shape = size(data)
+		num_channels = data_shape[N]
+		shape = data_shape[1:D]
+		num_neighbours_and_channels = M * num_channels
+
+		# Select only points for which the convolution kernel does not leave the data area
+		reduced_shape = shape .- kernelsize
+
+		# Allocate space
+		hankel = Array{ComplexF64, N}(undef, reduced_shape..., num_neighbours_and_channels)
+
+		one_index_shift = CartesianIndex(ntuple(d -> 1, Val(D))...)
+		i = 1 # counter for neighbours and num_channels
+		for L in neighbours
+			for c = 1:num_channels
+				for K in CartesianIndices(reduced_shape) # Iterate over spatial positions
+					# Shift to current position
+					κ = L + K - one_index_shift
+					# Extract from data
+					hankel[K, i] = data[L, c]
+				end
+				i += 1 # Next neighbour
+			end
+		end
+		# Flatten spatial dimensions to make it a matrix
+		return reshape(hankel, prod(reduced_shape), num_neighbours_and_channels)
+	end
+
+
+
 	"""
-		Last dimension assumed channels/contrast...
+		data = [spatial dims..., num_channels/contrast etc.]
+
+		Last dimension assumed num_channels/contrast...
 
 		tuples (channel, voxel, neighbour)
 
@@ -15,44 +52,14 @@ module MRIHankel
 		(1, 2, 1), (1, 2, 2), ...
 
 	"""
-	function hankel_matrix(
-		data::AbstractArray{<: Number, N}, # [spatial dims..., channels/contrast etc.]
-		kernelsize::NTuple{M, Integer}
-	) where {N,M}
-		@assert M == N - 1
-		@assert all(iseven, kernelsize)
-
-		# Get dimensions
-		data_shape = size(data)
-		kspace_shape = data_shape[1:M]
-		channels = data_shape[N]
-		neighbours = prod(kernelsize)
-		neighbours_and_channels = neighbours * channels
-
-		# Select only point for which the convolution kernel does not leave the calibration area
-		reduced_shape = kspace_shape .- kernelsize
-
-		# Allocate space
-		hankel = Array{ComplexF64, N}(undef, reduced_shape..., neighbours_and_channels)
-
-		one_index_shift = CartesianIndex(ntuple(d -> 1, Val(M))...)
-		for K in CartesianIndices(reduced_shape) # Iterate over spatial positions
-			# Reset counter for neighbours and channels
-			i = 1
-			for c = 1:channels
-				for L in CartesianIndices(kernelsize) # Iterate over neighbours
-					# Shift to current position
-					L += K - one_index_shift
-					# Extract from data
-					@inbounds hankel[K, i] = data[L, c]
-					# Next neighbour
-					i += 1
-				end
-			end
-		end
-		# Flatten spatial dimensions to make it a matrix
-		return reshape(hankel, prod(reduced_shape), neighbours_and_channels)
+	function hankel_matrix(data::AbstractArray{<: Number}, kernelsize::NTuple{D, Integer}) where D
+		@assert all(iseven, kernelsize) # TODO: Not sure why I did this
+		return hankel_matrix(data, kernelsize, CartesianIndices(kernelsize))
 	end
 
+	function hankel_matrix(data::AbstractArray{<: Number}, neighbours::NTuple{N, CartesianIndex{D}}) where {N, D}
+		kernelsize = Tuple(maximum(I[i] for I in neighbours) for i = 1:D)
+		return hankel_matrix(data, kernelsize, neighbours)
+	end
 end
 
